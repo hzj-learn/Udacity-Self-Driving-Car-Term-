5c4b4892f46820196fbfb64d7c46f4c9c3c43e2c
ubuntu-master <292224087@qq.com>
2022-05-16 12:23:17 +0800

    first publish

---
+++ src/main.cpp
@@ -0,0 +1,156 @@
+#include <uWS/uWS.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "json.hpp"
+#include "spline.h"
+#include <limits>
+#include "path_planning.h"
+
+// for convenience
+using nlohmann::json;
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+//   else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_first_of("}");
+  if (found_null != string::npos) {
+    return "";
+  } else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 2);
+  }
+  return "";
+}
+
+int main() {
+  uWS::Hub h;
+
+  // Load up map values for waypoint's x,y,s and d normalized normal vectors
+  vector<double> map_waypoints_x;
+  vector<double> map_waypoints_y;
+  vector<double> map_waypoints_s;
+  vector<double> map_waypoints_dx;
+  vector<double> map_waypoints_dy;
+
+  // Waypoint map to read from
+  string map_file_ = "../data/highway_map.csv";
+  // The max s value before wrapping around the track back to 0
+  double max_s = 6945.554;
+
+  std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
+
+  string line;
+  while (getline(in_map_, line)) {
+    std::istringstream iss(line);
+    double x;
+    double y;
+    float s;
+    float d_x;
+    float d_y;
+    iss >> x;
+    iss >> y;
+    iss >> s;
+    iss >> d_x;
+    iss >> d_y;
+    map_waypoints_x.push_back(x);
+    map_waypoints_y.push_back(y);
+    map_waypoints_s.push_back(s);
+    map_waypoints_dx.push_back(d_x);
+    map_waypoints_dy.push_back(d_y);
+  }
+  
+  PathPlanning planner;
+
+  Car car = {.x = 0.0, .y = 0.0, .s = 0.0, .d = 0.0, .yaw = 0.0, .speed = 0.0, .speed_ref = 0.0};
+
+  h.onMessage([&planner, &car, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy]
+              (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+               uWS::OpCode opCode) {
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+
+      auto s = hasData(data);
+
+      if (s != "") {
+        auto j = json::parse(s);
+        
+        string event = j[0].get<string>();
+        
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          
+          // Main car's localization Data
+          car = {.x = j[1]["x"], .y = j[1]["y"], .s = j[1]["s"], .d = j[1]["d"], .yaw = j[1]["yaw"], .speed = j[1]["speed"], .speed_ref = car.speed_ref};
+
+          //cout << "s: " << car.s << " , d: " << car.d << " , speed: " << car.speed << endl;
+
+          // Previous path data given to the Planner
+          auto previous_path_x = j[1]["previous_path_x"];
+          auto previous_path_y = j[1]["previous_path_y"];
+          // Previous path's end s and d values 
+          double end_path_s = j[1]["end_path_s"];
+          double end_path_d = j[1]["end_path_d"];
+
+          // Sensor Fusion Data, a list of all other cars on the same side 
+          //   of the road.
+          auto sensor_fusion = j[1]["sensor_fusion"];
+          
+          // Behavior selection
+          planner.behaviorSelection(car, sensor_fusion);
+
+          if (previous_path_x.size() > 0) {
+            car.s = end_path_s;
+          }
+          // Trajectory generation
+          vector<vector<double>> path_vals = planner.trajectoryGeneration(car, previous_path_x, \
+                                                      previous_path_y, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          /* Comment previous line and uncomment this one when you want to use manual mode in simulator.*/ 
+          //vector<vector<double>> path_vals = {{0.0},{0.0}};
+          json msgJson;
+          
+          msgJson["next_x"] = path_vals[0];
+          msgJson["next_y"] = path_vals[1];
+
+          auto msg = "42[\"control\","+ msgJson.dump()+"]";
+    
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }  // end "telemetry" if
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }  // end websocket if
+  }); // end h.onMessage
+
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
+
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                         char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
+
+  int port = 4567;
+  if (h.listen(port)) {
+    std::cout << "Listening to port " << port << std::endl;
+  } else {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  
+  h.run();
+}
---
+++ src/path_planning.cpp
@@ -0,0 +1,429 @@
+/**
+ * Path Planning class code
+ * Implementation of behavior planning and trajectory generation
+ * for the tour of an autonomous vechile in a high speed highway
+ * 
+ * Created on: March 15, 2019
+ * Author: Juan Rios
+ */
+
+#include "path_planning.h"
+
+#include <vector>
+#include <iostream>
+#include "json.hpp"
+#include "helpers.h"
+#include "spline.h"
+#include <limits>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using nlohmann::json;
+
+void PathPlanning::behaviorSelection(Car &car, json &sensor_fusion){
+
+    // Check if the car is in lane transition state and set the state false if the car is inside the expected new lane
+    if ((car.d > (2+4*lane-1)) && (car.d < (2+4*lane+1)) &&  changing_lane) {
+        changing_lane = false;
+        cout << "- Lane transition completed!" << endl;
+    }
+    
+    /** Sensor fusion information of closest vehicles ahead and behind by s value in all the lanes **/
+    // Store the s value of closest cars to vehicle that are ahead. The indexes correspond to the respective lane 0:left, 1:center and 2:right
+    vector<double> closest_s_ahead = {std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+    // Store the s value of closest cars to vehicle form behind in each lane. The indexes correspond to the respective lane 0:left, 1:center and 2:right
+    vector<double> closest_s_behind = {std::numeric_limits<double>::min(), std::numeric_limits<double>::min(), std::numeric_limits<double>::min()};
+    
+    // Store the s value of closest cars to vehicle that are ahead. The indexes correspond to the respective lane 0:left, 1:center and 2:right
+    vector<double> closest_speed_ahead = {std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+    // Store the s value of closest cars to vehicle form behind in each lane. The indexes correspond to the respective lane 0:left, 1:center and 2:right
+    vector<double> closest_speed_behind = {std::numeric_limits<double>::min(), std::numeric_limits<double>::min(), std::numeric_limits<double>::min()};
+    
+    // Store the s value of closest cars to vehicle that are ahead. The indexes correspond to the respective lane 0:left, 1:center and 2:right
+    vector<double> closest_d_ahead = {std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+    // Store the s value of closest cars to vehicle form behind in each lane. The indexes correspond to the respective lane 0:left, 1:center and 2:right
+    vector<double> closest_d_behind = {std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+
+    // Set current lane as fastest lane
+    int fastest_lane = lane;
+    
+    /** 1. SENSOR FUSION. Find the closest vehicles in every relevant direction **/
+    /**
+     * This loop is used to detect whether (1) there is a car too close ahead, (2) the left/right lanes
+     * are free of obstacles and (3) store the closest cars s values in each lane. (3) is used to later set
+     * the fastest_lane value and define that lane as the preferred direction during behavior selection
+     */ 
+    for (unsigned int i = 0; i < sensor_fusion.size(); ++i) {
+
+        // Identifier
+        int id = sensor_fusion[i][0];
+
+        // X component of speed for vehicle with index i
+        double vx = sensor_fusion[i][3];
+        // Y component of speed for vehicle with index i
+        double vy = sensor_fusion[i][4];
+        // Speed of vehicle with index i converted from MPH to m/s
+        double speed = sqrt(vx*vx + vy*vy)*(1609.0/3600.0);
+        // S position of vehicle with index i
+        double s = sensor_fusion[i][5];
+        // d value of vehicle with index i
+        float d = sensor_fusion[i][6];
+
+        /**
+         * Detects closest cars to the autonomous vehicle and their d, speed values in each lane. We are interested in the closest cars 
+         * ahead and behind the autonomous vehicle
+         */ 
+        if (abs(s-car.s)<120.0 && d > 0.0 && d < 12.0) {
+            int lane_temp = floor(d/4);
+            if (car.s < s) {
+                if (s < closest_s_ahead[lane_temp]) {
+                    closest_s_ahead[lane_temp] = s;
+                    closest_d_ahead[lane_temp] = d;
+                    closest_speed_ahead[lane_temp] = speed;
+                } 
+            } else {
+                if (s > closest_s_behind[lane_temp]) {
+                    closest_s_behind[lane_temp] = s;
+                    closest_d_behind[lane_temp] = d;
+                    closest_speed_behind[lane_temp] = speed;
+                } 
+            }
+        }  
+    }
+    
+    /** 2. DISTANCE LIMITS FLAGS. Finds whether there are vehicles too close ahead and behind **/
+    // Lane of car closest to the autonomous vehicle when considering an extended d range 
+    int closest_ahead_index = lane;
+    double temp = std::numeric_limits<double>::max();
+    for (unsigned int j = 0; j< closest_d_ahead.size(); ++j)
+    {
+        if ((closest_d_ahead[j] > (4*lane-0.75)) && (closest_d_ahead[j]<(4.75+4*lane))) {
+            if (temp > closest_s_ahead[j])
+            {
+                temp = closest_s_ahead[j];
+                closest_ahead_index = j;
+            }
+        }
+    }
+
+    if (closest_ahead_index != lane) {
+        cout << "  ! Vehicle coming to current lane from lane index: " << closest_ahead_index << endl;
+    }
+
+    // Minimum space gap required to keep a safe distance between autonomous vehicle and car ahead
+    // 5 meters is set as the minimum distance for the case when car speeds are the same.  
+    double min_safe_distance_ahead = std::max((car.speed_ref*car.speed_ref - closest_speed_ahead[closest_ahead_index]*closest_speed_ahead[closest_ahead_index]),0.0)/(2*9.5)+5.0;
+
+    // Car is ahead the autonomous vehicles is the distances is less than 20 Meters
+    if ((closest_s_ahead[closest_ahead_index]-car.s) < min_safe_distance_ahead) {
+        // If car is too close do some action    
+        if (!too_close[1]) too_close[1] = true;
+    } else {
+        if (too_close[1]) too_close[1] = false;
+    }
+
+    // Minimum space gap required to keep a safe distance between autonomous vehicle and car behind 
+    // 5 meters is set as the minimum distance for the case when car speeds are the same.   
+    double min_safe_distance_behind = std::max((closest_speed_behind[lane]*closest_speed_behind[lane] - car.speed_ref*car.speed_ref),0.0)/(2*9.5)+5.0;
+
+    if ((car.s - closest_s_behind[lane]) < min_safe_distance_behind) {
+        if (!too_close[0]) {
+            too_close[0] = true;
+            cout << "  ! Vehicle behind is too close." << endl;
+        }
+    } else {
+        if (too_close[0]) {
+            too_close[0] = false;
+            cout << "  ! Vehicle is safe behind." << endl;
+        }
+    }
+
+    /** 3. LANE SELECTION: Select next lane to transition or say in the same lane using future vehicle speed */
+    /** Identify whether right or left lanes are free of obstacles in the upper and lower right/left directions.
+     * This step is fundamental to then decide if a change of lane should be performed
+     */ 
+    // Lane transition possible directions: -1 to the left, +1 to the right
+    vector<int> lane_transition = {-1 , 1};
+    for (unsigned int i = 0; i < lane_transition.size(); ++i) {
+        
+        int temp_lane = lane + lane_transition[i];
+        
+        if (temp_lane >= 0 && temp_lane <=2) {
+            
+            
+            // Minimum space gap required to keep a safe distance between autonomous vehicle and car behind  
+            double min_safe_distance_behind = std::max((closest_speed_behind[temp_lane]*closest_speed_behind[temp_lane] - car.speed_ref*car.speed_ref),0.0)/(2*9.5)+5.0;
+            // Blocked at lower direction. It also tests whether the relative speeds are enough to mantain minimum safe distance after 2 seconds.
+            bool blocked_behind = !((car.s-closest_s_behind[temp_lane]+(car.speed_ref-closest_speed_behind[temp_lane]-9.5)*2.0) > min_safe_distance_behind);
+            
+            // Minimum space gap required to keep a safe distance between autonomous vehicle and car ahead  
+            double min_safe_distance_ahead = std::max((car.speed_ref*car.speed_ref - closest_speed_ahead[temp_lane]*closest_speed_ahead[temp_lane]),0.0)/(2*9.5)+5.0;
+            // Blocked at upper direction. It also tests whether the relative speeds are enough to mantain minimum safe distance after 2 seconds.
+            bool blocked_ahead = !((closest_s_ahead[temp_lane]-car.s+(closest_speed_ahead[temp_lane]+9.5-car.speed_ref)*2.0) > min_safe_distance_ahead);
+
+            if (blocked_behind) {
+                if (lane_transition[i]==1) {
+                    // Set too close as true for right lane transition in lower right direction
+                    too_close_right[0] = true;
+                } else {
+                    // Set too close as true for left lane transition in lower left direction
+                    too_close_left[0] = true;
+                }
+            } else {
+                if (lane_transition[i]==1) {
+                    // Set too close as false for right lane transition in lower right direction
+                    too_close_right[0] = false;
+                } else {
+                    // Set too close as false for left lane transition in lower left direction
+                    too_close_left[0] = false;
+                }
+            }
+
+            if (blocked_ahead) {
+                if (lane_transition[i]==1) {
+                    // Set too close as true for right lane transition in upper right direction
+                    too_close_right[1] = true;
+                } else {
+                    // Set too close as true for left lane transition in upper left direction
+                    too_close_left[1] = true;
+                }   
+            } else {
+                if (lane_transition[i]==1) {
+                    // Set too close as false for right lane transition in upper right direction
+                    too_close_right[1] = false;
+                } else {
+                    // Set too close as false for left lane transition in upper left direction
+                    too_close_left[1] = false;
+                }                   
+            }      
+        }
+    }
+
+    /**
+     * Here we define the fastest lane. The fastest lane is the one that is free of cars ahead
+     * or the maximum s distance ahead to the autonomous car
+     */ 
+    // If current lane is free of cars ahead, set that lane as the fastest one
+    if (closest_s_ahead[lane]==std::numeric_limits<double>::max()) {
+        fastest_lane = lane;
+    // Else, look for the maximum s distance. 
+    } else {
+        double s_temp = std::numeric_limits<double>::min();
+        for (unsigned int i = 0; i < closest_s_ahead.size(); ++i) {
+            if (s_temp < closest_s_ahead[i]) {
+                fastest_lane = i;
+                s_temp = closest_s_ahead[i];
+                // If a lane next to the current lane is free, define it as the fastest one and break the loop
+                if (((i == (lane-1)) || (i == (lane+1))) && (closest_s_ahead[i]==std::numeric_limits<double>::max())) break;
+            }
+        }
+    } 
+
+    /**
+     * Behavior selection: Change lane or stay in lane
+     */ 
+    // Use fastest lane direction as the preferred lane transition direction. Speed must be larger than 30 MPH for the lane transition to be allowed
+    if ((lane != fastest_lane) && car.speed > 30.0) {
+
+        bool free_left = lane>0 && !too_close_left[1] && !too_close_left[0] && !changing_lane;
+        bool free_right = lane<2 && !too_close_right[1] && !too_close_right[0] && !changing_lane;
+
+        int prev_lane = lane;
+        // If car ahead is too close consider changing lane.
+        if (too_close[1]) {
+            if (fastest_lane < lane) {
+                // If current lane is center or right, left lane is free for lane change and changing_lane flag is false       
+                if (free_left) {
+                    lane -= 1;
+                // Else, consider the right lane contraints
+                } else if (free_right) {
+                    lane +=1;
+                }
+            } else {
+                // If current lane is center or left, right lane is free for lane change and changing_lane flag is false   
+                if (free_right) {
+                    lane += 1;
+                // Else, consider the left lane contraints
+                } else if (free_left) {
+                    lane -= 1;
+                } 
+            }
+        // Else, there are not obstacles ahead but the fastest lane is not current lane
+        } else  {
+            // Consider moving in the direction of the fast lane 
+            if (fastest_lane < lane) {       
+                if (free_left) {
+                    lane -= 1;
+                } 
+            } else {
+                if (free_right) {       
+                    lane += 1;
+                } 
+            }
+        }
+        if (prev_lane!=lane) {
+            if (prev_lane > lane) std::cout << "+ Fastest lane: " << fastest_lane << ", going Left." << std::endl;
+            else std::cout << "+ Fastest lane: " << fastest_lane << ", going Right." << std::endl;
+            changing_lane = true;
+        }
+    }
+
+    /** 4. UPDATE SPEED BASED ON VEHICLE DISTANCE LIMITS FLAGS. **/
+    /**
+     * Behavior selection: Change speed.  If car ahead is too close reduce the speed, else increase speed
+     */
+    // Max speed of vehicle in m/s 
+    double max_speed_ms = 49.5*1609.0/3600.0;
+
+    if ((too_close[1]) && (car.speed_ref > 0.0))
+    {
+        // If car is 5 meters or less ahead reduce speed with an acceleration of 9.5m/s2
+        if ((closest_s_ahead[closest_ahead_index] - car.s) < 5.0) {
+            car.speed_ref += (-9.5*0.02);
+        // Else reduce speed with an acceleration enough to match the speed of current target 5 Meters before collision 
+        } else {
+            double target_accel = ((closest_speed_ahead[closest_ahead_index]*closest_speed_ahead[closest_ahead_index] \
+                                                    - car.speed_ref*car.speed_ref)/(2*(closest_s_ahead[closest_ahead_index]-car.s-5.0)));
+            if (target_accel < -9.5) target_accel = -9.5;
+            car.speed_ref += target_accel*0.02;
+        }
+    // If autonomous vehicle is too close to cars behind, speed up
+    } else if (too_close[0] && car.speed_ref <= (max_speed_ms-9.5*0.02)) {
+        // If car behind is too close increase speed with an acceleration of 9.5m/s2
+        car.speed_ref += (9.5*0.02);
+    // If autonomous vehicle is free behind and ahead, speed up
+    } else if (car.speed_ref <= (max_speed_ms-5.0*0.02)) {
+        car.speed_ref += (5.0*0.02);
+    }
+};
+
+vector<vector<double>> PathPlanning::trajectoryGeneration(Car &car, vector<double> previous_path_x, vector<double> previous_path_y, \
+                        vector<double> &map_waypoints_s, vector<double> &map_waypoints_x, vector<double> &map_waypoints_y) {
+    
+    vector<vector<double>> path_vals;
+
+    // Create a list of widely spaced (x,y) waypoints, evenly spaces at 30 m
+    // We will interpolate this points using the spline method
+    vector<double> ptsx;
+    vector<double> ptsy;
+
+    // Reference x, y and yaw states
+    // We will reference the current car position or we will access the last path end point
+    double ref_x = car.x;
+    double ref_y = car.y;
+    double ref_yaw = deg2rad(car.yaw);
+
+    // If the previous path is almost empty, use the car as starting reference
+    int prev_size = previous_path_x.size();
+    if ( prev_size < 2) {
+        double prev_car_x = car.x - cos(car.yaw);
+        double prev_car_y = car.y - sin(car.yaw);  
+
+        ptsx.push_back(prev_car_x);
+        ptsx.push_back(car.x);
+
+        ptsy.push_back(prev_car_y);
+        ptsy.push_back(car.y);
+    } 
+    // Use previous path points as starting reference
+    else {
+        
+        ref_x = previous_path_x[prev_size-1];
+        ref_y = previous_path_y[prev_size-1];
+
+        double ref_x_prev = previous_path_x[prev_size-2];
+        double ref_y_prev = previous_path_y[prev_size-2];
+        
+        // Define current vehicle orientation from its two previous point locations
+        ref_yaw = atan2((ref_y-ref_y_prev),(ref_x-ref_x_prev));
+
+        ptsx.push_back(ref_x_prev);
+        ptsx.push_back(ref_x);
+
+        ptsy.push_back(ref_y_prev);
+        ptsy.push_back(ref_y);
+    }
+
+    double step_s = 32.0;
+
+    // Add evenly spaces points 30 m ahead of the starting point in frenet coordinates
+    vector<double> next_wp0 = getXY(car.s+step_s*1, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y); //28
+    vector<double> next_wp1 = getXY(car.s+step_s*2, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+    vector<double> next_wp2 = getXY(car.s+step_s*3, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+    ptsx.push_back(next_wp0[0]);
+    ptsx.push_back(next_wp1[0]);
+    ptsx.push_back(next_wp2[0]);
+
+    ptsy.push_back(next_wp0[1]);
+    ptsy.push_back(next_wp1[1]);
+    ptsy.push_back(next_wp2[1]);
+
+    /**
+     * Apply translation and rotation transformation to move 
+     * points from the global coordinate system to the local car coordinate
+     * system
+     */ 
+    for (unsigned int i = 0; i < ptsx.size(); ++i) {
+        // Shift car reference to origin and angle to 0 degrees
+        double shift_x = ptsx[i] - ref_x;
+        double shift_y = ptsy[i] - ref_y;
+
+        ptsx[i] = (shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+        ptsy[i] = (shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+    }
+
+    // Create a spline  
+    tk::spline s;
+
+    // Set (x,y) points to the spline
+    s.set_points(ptsx, ptsy);
+
+    // Start with all of the previous path points of last time
+    vector<double> next_x_vals;
+    vector<double> next_y_vals;
+
+    for (unsigned int i =0; i < previous_path_x.size(); ++i) {
+        next_x_vals.push_back(previous_path_x[i]);
+        next_y_vals.push_back(previous_path_y[i]);
+    }
+
+    // Calculate how to break up spline points so that we travel at our desired reference speed
+    double target_x = step_s;
+    double target_y = s(target_x);
+    double target_distance = sqrt(target_x*target_x + target_y*target_y);
+
+    double x_add_on = 0.0;    
+
+    // Define N, the number of evenly transitions expected to guarantee an average speed ot ref_vel in the next
+    // 30 Meters
+    double N = target_distance / (0.02*car.speed_ref);
+    double x_point;
+    double y_point;
+
+    for (unsigned int i = 1; i < 50-previous_path_x.size(); ++i) {
+
+        x_point = x_add_on + target_x/N;
+        y_point = s(x_point);  
+
+        x_add_on = x_point;
+
+        double x_temp = x_point;
+        double y_temp = y_point;
+
+        x_point = (x_temp*cos(ref_yaw) - y_temp*sin(ref_yaw));
+        y_point =  (x_temp*sin(ref_yaw) + y_temp*cos(ref_yaw));
+
+        x_point += ref_x;
+        y_point += ref_y;
+
+        next_x_vals.push_back(x_point);
+        next_y_vals.push_back(y_point);
+    }
+
+    path_vals.push_back(next_x_vals);
+    path_vals.push_back(next_y_vals);
+
+    return path_vals;
+};
